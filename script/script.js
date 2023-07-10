ac68956104c8de0f470d7b64f3516c9f6aaf6657
Enofua Divine <divineenofua@gmail.com>
2023-07-10 17:32:14 +0100

    modified course page

--- script/script.js
+++ script/script.js
@@ -4,6 +4,39 @@ const navMenu = document.getElementById("nav-class");
 const closePageBtn = document.getElementById("close-btn");
 const logInPage = document.getElementById("login-page");
 const logInbtn = document.getElementById("login-btn");
+const searchBtn = document.getElementById("search");
+const hiddenSection = document.getElementById("hidden");
+const closehiddenSection = document.getElementById("close-hide");
+const progressBar = document.getElementById("angle-id");
+const  courseProgress = document.querySelector('.course-progress');
+const  courseProgressClass = document.querySelectorAll(".uil-angle-down");
+
+// progressBar.addEventListener("click", () => {
+//   courseProgress.classList.toggle("active");
+// // courseProgress.style.display = 'block';
+// })
+
+ courseProgressClass.forEach(show => {
+show.addEventListener("click", () => {
+    courseProgress.classList.toggle("active");
+
+    // courseProgress.classList.toggle("active");
+})
+})
+
+
+
+
+
+
+searchBtn.addEventListener("click", () => {
+    hiddenSection.style.display = "block";
+     
+   })
+closehiddenSection.addEventListener("click", () => {
+    hiddenSection.style.display = "none";
+     
+   })
 
 openMenu.addEventListener("click", () => {
  navMenu.style.display = "flex";
